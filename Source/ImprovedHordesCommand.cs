4a3dc78868a8fbd18fee3d0af349ef8278f60cb4
Filip Jerkovic <filjerkovic@gmail.com>
2021-12-30 22:35:13 +1300

    Refactoring

--- Source/ImprovedHordesCommand.cs
+++ Source/ImprovedHordesCommand.cs
@@ -14,90 +14,111 @@ namespace ImprovedHordes
             {
                 if (_params[0].EqualsCaseInsensitive("wandering"))
                 {
-                    if (_params.Count >= 2)
-                    {
-                        var wanderingHorde = ImprovedHordesManager.Instance.WanderingHorde;
-
-                        if (_params[1].EqualsCaseInsensitive("spawn"))
-                        {
-                            bool feral = false;
-                            if(_params.Count >= 3)
-                            {
-                                if(_params[2].EqualsCaseInsensitive("feral"))
-                                {
-                                    feral = true;
-                                }
-                            }
-
-                            wanderingHorde.ForceSpawnWanderingHorde(feral);
-
-                            SingletonMonoBehaviour<SdtdConsole>.Instance.Output("[Improved Hordes] Wandering Hordes spawning for all groups.");
-                        }
-                        else if (_params[1].EqualsCaseInsensitive("reset"))
-                        {
-                            wanderingHorde.state = WanderingHordeManager.EHordeState.Finished;
-                            wanderingHorde.schedule.Reset();
-
-                            SingletonMonoBehaviour<SdtdConsole>.Instance.Output("[Improved Hordes] Wandering Horde weekly schedule reset.");
-                        }
-                        else if (_params[1].EqualsCaseInsensitive("show"))
-                        {
-                            var schedule = wanderingHorde.schedule;
-
-                            StringBuilder builder = new StringBuilder();
-                            builder.AppendLine("Settings:");
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.DAYS_PER_RESET), schedule.DAYS_PER_RESET));
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_TO_FIRST_OCCURANCE_MIN), schedule.HOURS_TO_FIRST_OCCURANCE_MIN));
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_IN_WEEK_FOR_LAST_OCCURANCE_MAX), schedule.HOURS_IN_WEEK_FOR_LAST_OCCURANCE_MAX));
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_APART_MIN), schedule.HOURS_APART_MIN));
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.MIN_OCCURANCES), schedule.MIN_OCCURANCES));
-                            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.MAX_OCCURANCES), schedule.MAX_OCCURANCES));
-                            builder.AppendLine("");
-                            builder.AppendLine("Schedule:");
-                            for (int i = 0; i < schedule.occurances.Count; i++)
-                            {
-                                var occurance = schedule.occurances[i];
-                                var worldTime = occurance.worldTime;
-                                var feral = occurance.feral;
-
-                                var (Days, Hours, Minutes) = GameUtils.WorldTimeToElements(worldTime);
-                                builder.AppendLine(String.Format("- {0} at Day {1} {2:D2} {3:D2} ({4})", i + 1, Days, Hours, Minutes, feral ? "Feral" : "Not Feral"));
-                            }
-
-                            builder.AppendLine("");
-
-                            if (wanderingHorde.state == WanderingHordeManager.EHordeState.Finished)
-                            {
-                                if (schedule.currentOccurance < schedule.occurances.Count)
-                                    builder.AppendLine(String.Format("Next Occurance {0} ", schedule.currentOccurance + 1));
-                                else
-                                    builder.AppendLine("No more occurances this week.");
-                            }
-                            else
-                            {
-                                builder.AppendLine(String.Format("Current Occurance: {0}", schedule.currentOccurance + 1));
-                                builder.AppendLine(String.Format("State: {0}", Enum.GetName(typeof(WanderingHordeManager.EHordeState), wanderingHorde.state)));
-                            }
-
-                            builder.AppendLine("");
-
-                            var resetWorldTime = schedule.nextResetTime;
-                            var resetWorldTimeElements = GameUtils.WorldTimeToElements(resetWorldTime);
-                            builder.AppendLine(String.Format("Next reset at Day {0} {1:D2} {2:D2}", resetWorldTimeElements.Days, resetWorldTimeElements.Hours, resetWorldTimeElements.Minutes));
-
-                            SingletonMonoBehaviour<SdtdConsole>.Instance.Output(builder.ToString());
-                        }
-                    }
-                    else
-                    {
-                        SingletonMonoBehaviour<SdtdConsole>.Instance.Output("No sub command given.");
-                    }
+                    this.ExecuteWandering(_params);
                 }
             }
             else
             {
                 SingletonMonoBehaviour<SdtdConsole>.Instance.Output("No sub command given.");
             }
+        }
+
+        private void ExecuteWandering(List<string> _params)
+        {
+            if (_params.Count >= 2)
+            {
+                if (_params[1].EqualsCaseInsensitive("spawn"))
+                {
+                    this.ExecuteWanderingSpawn(_params);
+                }
+                else if (_params[1].EqualsCaseInsensitive("reset"))
+                {
+                    this.ExecuteWanderingReset(_params);
+                }
+                else if (_params[1].EqualsCaseInsensitive("show"))
+                {
+                    this.ExecuteWanderingShow(_params);
+                }
+            }
+            else
+            {
+                SingletonMonoBehaviour<SdtdConsole>.Instance.Output("No sub command given.");
+            }
+        }
+
+        private void ExecuteWanderingSpawn(List<string> _params)
+        {
+            bool feral = false;
+            if (_params.Count >= 3)
+            {
+                if (_params[2].EqualsCaseInsensitive("feral"))
+                {
+                    feral = true;
+                }
+            }
+
+            ImprovedHordesManager.Instance.WanderingHorde.ForceSpawnWanderingHorde(feral);
+
+            SingletonMonoBehaviour<SdtdConsole>.Instance.Output("[Improved Hordes] Wandering Hordes spawning for all groups.");
+        }
+
+        private void ExecuteWanderingReset(List<string> _params)
+        {
+            var wanderingHorde = ImprovedHordesManager.Instance.WanderingHorde;
+
+            wanderingHorde.state = WanderingHordeManager.EHordeState.Finished;
+            wanderingHorde.schedule.Reset();
+
+            SingletonMonoBehaviour<SdtdConsole>.Instance.Output("[Improved Hordes] Wandering Horde weekly schedule reset.");
+        }
+
+        private void ExecuteWanderingShow(List<string> _params)
+        {
+            var wanderingHorde = ImprovedHordesManager.Instance.WanderingHorde;
+            var schedule = wanderingHorde.schedule;
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Settings:");
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.DAYS_PER_RESET), schedule.DAYS_PER_RESET));
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_TO_FIRST_OCCURANCE_MIN), schedule.HOURS_TO_FIRST_OCCURANCE_MIN));
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_IN_WEEK_FOR_LAST_OCCURANCE_MAX), schedule.HOURS_IN_WEEK_FOR_LAST_OCCURANCE_MAX));
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.HOURS_APART_MIN), schedule.HOURS_APART_MIN));
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.MIN_OCCURANCES), schedule.MIN_OCCURANCES));
+            builder.AppendLine(String.Format("{0}: {1}", nameof(WanderingHordeSchedule.MAX_OCCURANCES), schedule.MAX_OCCURANCES));
+            builder.AppendLine("");
+            builder.AppendLine("Schedule:");
+            for (int i = 0; i < schedule.occurances.Count; i++)
+            {
+                var occurance = schedule.occurances[i];
+                var worldTime = occurance.worldTime;
+                var feral = occurance.feral;
+
+                var (Days, Hours, Minutes) = GameUtils.WorldTimeToElements(worldTime);
+                builder.AppendLine(String.Format("- {0} at Day {1} {2:D2} {3:D2} ({4})", i + 1, Days, Hours, Minutes, feral ? "Feral" : "Not Feral"));
+            }
+
+            builder.AppendLine("");
+
+            if (wanderingHorde.state == WanderingHordeManager.EHordeState.Finished)
+            {
+                if (schedule.currentOccurance < schedule.occurances.Count)
+                    builder.AppendLine(String.Format("Next Occurance {0} ", schedule.currentOccurance + 1));
+                else
+                    builder.AppendLine("No more occurances this week.");
+            }
+            else
+            {
+                builder.AppendLine(String.Format("Current Occurance: {0}", schedule.currentOccurance + 1));
+                builder.AppendLine(String.Format("State: {0}", Enum.GetName(typeof(WanderingHordeManager.EHordeState), wanderingHorde.state)));
+            }
+
+            builder.AppendLine("");
+
+            var resetWorldTime = schedule.nextResetTime;
+            var resetWorldTimeElements = GameUtils.WorldTimeToElements(resetWorldTime);
+            builder.AppendLine(String.Format("Next reset at Day {0} {1:D2} {2:D2}", resetWorldTimeElements.Days, resetWorldTimeElements.Hours, resetWorldTimeElements.Minutes));
+
+            SingletonMonoBehaviour<SdtdConsole>.Instance.Output(builder.ToString());
         }
 
         public override string[] GetCommands()
@@ -107,7 +128,7 @@ namespace ImprovedHordes
 
         public override string GetDescription()
         {
-            return "Execute a function from the Improved Hordes Server Mod. help improvedhordes - for more information.";
+            return "Execute a function from the Improved Hordes Mod. `help improvedhordes` for more information.";
         }
 
         public override string GetHelp()
