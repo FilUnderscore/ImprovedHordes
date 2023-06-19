2780bf60b0db6ef9ce24cc68b0cd0cee8808bb88
FilUnderscore <fil@filunderscore.com>
2023-06-20 00:05:16 +1200

    World event report interest tweak.

--- Source/ImprovedHordes/Core/World/Event/WorldEventReporter.cs
+++ Source/ImprovedHordes/Core/World/Event/WorldEventReporter.cs
@@ -166,7 +166,7 @@ namespace ImprovedHordes.Core.World.Event
 
             static void Postfix(AIDirectorChunkEvent _chunkEvent)
             {
-                WorldEventReporter.Report(new WorldEvent(_chunkEvent.Position, _chunkEvent.Value * 4));
+                WorldEventReporter.Report(new WorldEvent(_chunkEvent.Position, _chunkEvent.Value * 2));
             }
         }
     }
