f315f3779188ff46284b02f65e71eb3b6ae21910
FilUnderscore <fil@filunderscore.com>
2023-08-05 01:39:17 +1200

    Remove unused import.

--- Source/ImprovedHordes/Core/World/Horde/WorldHordeTracker.cs
+++ Source/ImprovedHordes/Core/World/Horde/WorldHordeTracker.cs
@@ -17,7 +17,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace ImprovedHordes.Core.World.Horde
